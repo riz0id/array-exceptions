6dcca36f1458fba7ed8214a9e87d8f3530a7d8dd
Jake Leach <jacobleach@protonmail.com>
2022-12-01 08:36:39 -0600

    add: `AllocError` exception

--- src/Control/Exception/Array.hs
+++ src/Control/Exception/Array.hs
@@ -14,11 +14,15 @@
 --
 -- @since 1.0.0
 module Control.Exception.Array (
-  -- * ElementError 
+  -- * Allocation Exceptions
+  AllocError (..),
+  showAllocErrorMessage,
+
+  -- * Undefined Element Exceptions
   ElementError (ElementErrorWithIndex, ElementError, ..),
   showElementErrorMessage,
 
-  -- * IndexError
+  -- * Out of Bounds Indexing Exceptions
   IndexError (..),
   showIndexErrorMessage,
 ) where
@@ -31,62 +35,128 @@ import GHC.Generics (Generic)
 
 import Language.Haskell.TH.Syntax (Lift)
 
--- ElementError ----------------------------------------------------------------
+-- Allocation Exceptions -------------------------------------------------------
 
 -- | TODO
 --
 -- @since 1.0.0
-newtype ElementError = MkElementError 
+data AllocError
+  = -- | TODO: docs
+    AllocError Int
+  | -- | TODO: docs
+    AlignError Int
+  deriving (Data, Eq, Generic, Lift, Ord)
+
+-- | Renders the 'AllocError' via 'showAllocErrorMessage'.
+--
+-- @since 1.0.0
+instance Exception AllocError where
+  displayException = showAllocErrorMessage
+  {-# INLINE displayException #-}
+
+-- | Renders the 'AllocError' via 'showAllocErrorMessage'.
+--
+-- @since 1.0.0
+instance Show AllocError where
+  showsPrec _ exn = showString (showAllocErrorMessage exn)
+  {-# INLINE showsPrec #-}
+
+-- | The implementation for 'displayException'. Renders the 'AllocError' as a
+-- human-readable string. 
+--
+-- For v'AllocError' exceptions, the rendered error message is:
+-- 
+-- >>> showAllocErrorMessage (AllocError (- 5))
+-- "AllocError: failed to allocate array of length -5"
+--
+-- For v'AlignError' exceptions, the rendered error message is:
+--
+-- >>> showAllocErrorMessage (AlignError 0)
+-- "AlignError: cannot allocate array aligned to 0 bytes"
+--
+-- @since 1.0.0
+showAllocErrorMessage :: AllocError -> String
+showAllocErrorMessage (AllocError n) = 
+  "AllocError: failed to allocate array of length " ++ show n
+showAllocErrorMessage (AlignError n) = 
+  "AlignError: cannot allocate array aligned to " ++ shows n " bytes"
+
+-- Undefined Element Exceptions ------------------------------------------------
+
+-- | 'ElementError' represents an exception that is used to report when an 
+-- undefined array element is evaluated.  
+--
+-- @since 1.0.0
+newtype ElementError = MkElementError
   {getElementError :: Maybe Int}
   deriving (Data, Eq, Generic, Lift, Ord)
 
--- | TODO
+-- | The 'ElementErrorWithIndex' pattern constructs an 'ElementError' with the  
+-- index of the array element that triggered the exception.
 --
 -- @since 1.0.0
-pattern ElementErrorWithIndex :: Int -> ElementError 
+pattern ElementErrorWithIndex :: Int -> ElementError
 pattern ElementErrorWithIndex i = MkElementError (Just i)
 
--- | TODO
+-- | The 'ElementError' pattern constructs an 'ElementError' for an unknown or 
+-- unspecified array index.
 --
 -- @since 1.0.0
-pattern ElementError :: ElementError 
+pattern ElementError :: ElementError
 pattern ElementError = MkElementError Nothing
 
 {-# COMPLETE ElementErrorWithIndex, ElementError #-}
 
--- | Renders the 'IndexError' via 'showIndexErrorMessage'.
+-- | Renders the 'ElementError' via 'showElementErrorMessage'.
 --
 -- @since 1.0.0
 instance Exception ElementError where
   displayException = showElementErrorMessage
   {-# INLINE displayException #-}
 
--- | Renders the 'IndexError' via 'showIndexErrorMessage'.
+-- | Renders the 'ElementError' via 'showElementErrorMessage'.
 --
 -- @since 1.0.0
-instance Show ElementError where 
+instance Show ElementError where
   showsPrec _ exn = showString (showElementErrorMessage exn)
   {-# INLINE showsPrec #-}
 
--- | The implementation for 'displayException'. Renders the 'IndexError' as a
--- human-readable string.
+-- | The implementation for 'displayException'. Renders the 'ElementError' as a
+-- human-readable string. 
+--
+-- If the 'ElementError' is given the array index of the element that triggered
+-- the exception (i.e. the 'ElementError' was constructed via an
+-- 'ElementErrorWithIndex' pattern), then the array index is noted in rendered 
+-- error message: 
+--
+-- >>> ElementErrorWithIndex 5
+-- ElementError: evaluated undefined array element at index 5
+--
+-- If the index of the array element is unknown or unspecified (i.e. the 
+-- 'ElementError' was constructed via an v'ElementError' pattern), then the 
+-- rendered error message is:
+--
+-- >>> ElementError
+-- ElementError: evaluated undefined array element
+--
+-- @since 1.0.0
 showElementErrorMessage :: ElementError -> String
-showElementErrorMessage (ElementErrorWithIndex i) = 
+showElementErrorMessage (ElementErrorWithIndex i) =
   "ElementError: evaluated undefined array element at index " ++ show i
-showElementErrorMessage ElementError = 
-  "ElementError: evaluated undefined array element" 
+showElementErrorMessage ElementError =
+  "ElementError: evaluated undefined array element"
 
--- IndexError ------------------------------------------------------------------
+-- Out of Bounds Indexing Exceptions -------------------------------------------
 
--- | 'IndexError' represents an exception that is raised when an out-of-bounds 
+-- | 'IndexError' represents an exception that is raised when an out-of-bounds
 -- index would be used to access an array.
 --
 -- @since 1.0.0
 data IndexError = IndexError
   { arrayIndex :: Int
-  -- ^ The offending index. 
+  -- ^ The offending index.
   , arrayLength :: Int
-  -- ^ The length of the array. 
+  -- ^ The length of the array.
   }
   deriving (Data, Eq, Generic, Lift, Ord)
 
@@ -105,7 +175,7 @@ instance Show IndexError where
   {-# INLINE showsPrec #-}
 
 -- | The implementation for 'displayException'. Renders the 'IndexError' as a
--- human-readable string.
+-- human-readable string. 
 --
 -- >>> showIndexErrorMessage (IndexError 5 3)
 -- "IndexError: index out of bounds: length is 3 but the index is 5"
